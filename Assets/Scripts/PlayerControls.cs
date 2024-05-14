46e3328e003023ca8d293392f68351325d6e706c
Bugraalgan <bugraallgan@gmail.com>
2024-05-14 18:48:38 +0300

    Argon
    commit 2

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+        
+    }
+}
